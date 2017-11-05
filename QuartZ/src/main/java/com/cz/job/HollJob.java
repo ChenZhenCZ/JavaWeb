940c5c23468ef0e310a60c5b585c050b6f48f685
ChenZhenCZ <2908903432@qq.com>
2017-11-05 19:40:28 +0800

    quart定时任务

---
+++ QuartZ/src/main/java/com/cz/job/HollJob.java
@@ -0,0 +1,11 @@
+package com.cz.job;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HollJob {
+
+    public  void run(){
+        System.out.println("启动111111！");
+    }
+}
---
+++ QuartZ/src/main/java/com/cz/job/Test.java
@@ -0,0 +1,11 @@
+package com.cz.job;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+    public void run(){
+        System.out.println("启动222222222222！");
+    }
+}
